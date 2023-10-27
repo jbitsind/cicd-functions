3c2253bdcfd02aa07bf9b23989100749b7b6af08
jbitsind <jbitsind@gmail.com>
2023-10-27 22:09:41 +0000

    Implemented the word sorting Azure Function

--- src/test/java/edu/cmu/cs/devops/FunctionTest.java
+++ src/test/java/edu/cmu/cs/devops/FunctionTest.java
@@ -1,44 +1,48 @@
 package edu.cmu.cs.devops;
 
-import com.microsoft.azure.functions.*;
-import org.mockito.invocation.InvocationOnMock;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import org.apache.commons.codec.binary.Base64;
+import org.junit.jupiter.api.Test;
 import org.mockito.stubbing.Answer;
 
-import java.util.*;
+import java.util.Optional;
 import java.util.logging.Logger;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 
 /**
  * Unit test for Function class.
  */
 public class FunctionTest {
+
+    private final Base64 base64 = new Base64();
+
+    private static final String PROPER_REQUEST_INPUT = "Hello from DevOps! Hi from Azure! 1sn't th1$ fun? 4";
+    private static final String PROPER_REQUEST_OUTPUT = "1sn't 4 Azure! DevOps! from from fun? Hello Hi th1$";
+
     /**
      * Unit test for HttpTriggerJava method.
      */
     @Test
-    public void testHttpTriggerJava() throws Exception {
+    public void testHttpTriggerJavaForProperInput() {
         // Setup
         @SuppressWarnings("unchecked")
         final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
 
-        final Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("name", "Azure");
-        doReturn(queryParams).when(req).getQueryParameters();
-
-        final Optional<String> queryBody = Optional.empty();
+        final Optional<String> queryBody = Optional.of(base64.encodeAsString(PROPER_REQUEST_INPUT.getBytes()));
         doReturn(queryBody).when(req).getBody();
 
-        doAnswer(new Answer<HttpResponseMessage.Builder>() {
-            @Override
-            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
-                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
-                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
-            }
+        doAnswer((Answer<HttpResponseMessage.Builder>) invocation -> {
+            HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+            return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
         }).when(req).createResponseBuilder(any(HttpStatus.class));
 
         final ExecutionContext context = mock(ExecutionContext.class);
@@ -49,5 +53,32 @@ public class FunctionTest {
 
         // Verify
         assertEquals(ret.getStatus(), HttpStatus.OK);
+        assertEquals(ret.getBody().toString(), base64.encodeAsString(PROPER_REQUEST_OUTPUT.getBytes()));
     }
+
+    @Test
+    public void testHttpTriggerJavaForImproperInput() {
+        // Setup
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        final Optional<String> queryBody = Optional.empty();
+        doReturn(queryBody).when(req).getBody();
+
+        doAnswer((Answer<HttpResponseMessage.Builder>) invocation -> {
+            HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+            return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(req, context);
+
+        // Verify
+        assertEquals(ret.getStatus(), HttpStatus.BAD_REQUEST);
+    }
+
+
 }
